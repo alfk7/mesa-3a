d14d7132c6cd743f34c87924d9d253f3846a7956
MikeInhota <mike.batista@etecia.com.br>
2020-02-03 17:06:42 -0300

    Todas as informações

---
+++ meuGrupo.js
@@ -0,0 +1,19 @@
+let equipe = {
+    nome: "Alef",
+    passaporte: "ir a praia",
+    esporte: "volei",
+    membros_do_grupo: ["Alef","Marcos","Mike"]
+}
+let marcos = {
+    Nome:"Marcos",
+    Passatempo:"games ",
+    Esporte:"futebol",
+    MembrosDoGrupo: ["alef","marcos","mike"]
+
+};
+const meuNome = {
+    nome: "Mike",
+    Passatempo: "Jogar RPG",
+    Esporte: "Basquete",
+    MembrosDoGrupo: ["Mike", "Alef", "Marcos"],
+}
